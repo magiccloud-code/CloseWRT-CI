74b56973802142f08cbecef92993994293e69598
magiccloud-code <magiccloud89@gmail.com>
2026-09-04 12:44:45 +0800

    try fix

--- Scripts/PRIVATE.sh
+++ Scripts/PRIVATE.sh
@@ -2,4 +2,4 @@ sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns} feeds/packages/utils/v2dat feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang -b 1.26 feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
