28280140d62a1704daa976dade6ec9e888dc0d01
magiccloud-code <magiccloud89@gmail.com>
2026-09-10 20:20:24 +0800

    Modify homeproxy package update with new dependency
    Updated homeproxy package to include 'sing-box' dependency.

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -63,7 +63,7 @@ UPDATE_PACKAGE "passwall" "Openwrt-Passwall/openwrt-passwall" "main" "pkg"
 UPDATE_PACKAGE "passwall2" "Openwrt-Passwall/openwrt-passwall2" "main" "pkg"
 UPDATE_PACKAGE "clashoo" "kenzok8/openwrt-clashoo" "main"
 UPDATE_PACKAGE "daede" "kenzok8/openwrt-daede" "main" "" "luci-app-daede daed dae"
-UPDATE_PACKAGE "homeproxy" "XiaoHaiSly/luci-app-homeproxy" "main" "" "luci-app-homeproxy"
+UPDATE_PACKAGE "homeproxy" "XiaoHaiSly/luci-app-homeproxy" "main" "" "luci-app-homeproxy sing-box"
 
 UPDATE_PACKAGE "ddns-go" "sirpdboy/luci-app-ddns-go" "main"
 UPDATE_PACKAGE "diskman" "sbwml/luci-app-diskman" "main"
